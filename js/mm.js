db823b1f08f631a8a0480e1f95156f73f742a69b
Olivia Glennon <olivia@fathom.info>
2015-03-12 12:01:02 -0400

    cleaning up and adding comments

--- js/mm.js
+++ js/mm.js
@@ -5,15 +5,14 @@ $(document).ready(function(){
 
   var width = 900;
   var height = 600;
-  var view = "regions";
 
   var pts = [];
   var keyLabels = [];
-  var regions = [ "EAE", "ECE", "LCE", "MEE", "NAE", "SSE", "SAE", "GLE" ];
+  var regions = [ "EAE", "ECE", "LCE", "MEE", "NAE", "SSE", "SAE", "GLE" ]; // array of which regions we want to show
 
   var s = function( p ) {
 
-    var iso2color = {
+    var iso2color = { // set up colors for different regions
       'none': p.color(0),
       'ECE': p.color(118, 91, 160),
       'EAE': p.color(200, 70, 70),
@@ -49,10 +48,10 @@ $(document).ready(function(){
         this.hovered = false;
       }
 
-      if(keyHover == '' || keyHover == this.ISO){
+      if(keyHover == '' || keyHover == this.ISO){ // check if point is part of current active region or no active region is selected
         p.fill(this.color);
       } else {
-        p.fill(0,10);
+        p.fill(0,10); // if not in the selected/hovered region, draw as transparent grey
       }
       p.ellipse(this.cx, this.cy, this.r, this.r);
     }
@@ -71,11 +70,13 @@ $(document).ready(function(){
 
     PointLabel.prototype.display = function(){
       var hovered = [];
-      for(var h = 0; h < pts.length; h++){
+      for(var h = 0; h < pts.length; h++){ // make an array of all the points being hovered (allows us to check for overlapping points)
         if(pts[h].hovered === true){
           hovered.push(pts[h]);
         }
       }
+
+      // check if point is being hovered, and if so, show the label
       if( p.mouseX <= (this.cx + this.r)
           && p.mouseX >= (this.cx - this.r)
           && p.mouseY <= (this.cy + this.r)
@@ -114,76 +115,12 @@ $(document).ready(function(){
       p.text(this.name, this.x, this.y);
     }
 
-    function drawISOLabel(){
-      p.textAlign(p.LEFT);
-      // console.log(keyHover);
-      var firstYear = '';
-      if(keyHover == ''){
-        for(var pt = 0; pt < pts.length; pt++){
-          if(pts[pt].ISO == 'GLE' && pts[pt].year == 1995){
-            firstYear = pts[pt];
-          }
-          if(pts[pt].ISO == 'GLE' && pts[pt].year == 2010){
-            var direction = '';
-            var perc = ((pts[pt].value - firstYear.value)/firstYear.value)*100;
-            if(perc < 0){
-              direction = '-';
-            } else if (perc > 0){
-              direction = '+';
-            }
-            p.fill(pts[pt].color);
-            p.text(iso2name[pts[pt].ISO] + ' ' + direction + Math.abs(Math.round(perc)) + '%', pts[pt].cx + 10, pts[pt].cy - 10, 100, 30);
-          }
-        }
-      } else {
-        for(var r = 0; r < regions.length; r++){
-          if(keyHover == regions[r]){
-            for(var pt = 0; pt < pts.length; pt++){
-              if(pts[pt].ISO == keyHover && pts[pt].year == 1995){
-                firstYear = pts[pt];
-              }
-              if(pts[pt].ISO == keyHover && pts[pt].year == 2010){
-                var direction = '';
-                var perc = ((pts[pt].value - firstYear.value)/firstYear.value)*100;
-                if(perc < 0){
-                  direction = '-';
-                } else if (perc > 0){
-                  direction = '+';
-                }
-                p.fill(pts[pt].color);
-                p.text(iso2name[pts[pt].ISO] + ' ' + direction + Math.abs(Math.round(perc)) + '%', pts[pt].cx + 10, pts[pt].cy - 10, 100, 30);
-              }
-            }
-          }
-        }
-      }
-    }
-
-    function getCX(yr){
-      return p.map(yr,1995,2010,0+150,width-200);
-    }
-
-    function getCY(val){
-      return p.map(val,0,800,height-50,0+50);
-    }
-
-    p.drawPointLabels = function(ISO){
-      var d = data[ISO];
-      var isoLabels = [];
-      for (var yr in d){
-        var newLabel = new PointLabel(yr, d[yr], ISO);
-        isoLabels.push(newLabel);
-      }
-      
-      for(var pt = 0; pt < isoLabels.length; pt++){
-        isoLabels[pt].display();
-      }
-    }
-
     p.drawRegions = function() {
       p.stroke(235);
       p.strokeWeight(1);
       p.noFill();
+
+      // draw axes
       p.line(getCX(1995),height-50,getCX(2010) + 5,height-50);
       for(var yr = 1995; yr <= 2010; yr = yr+5){
         p.line(getCX(yr),0+50,getCX(yr),height-45);
@@ -205,14 +142,20 @@ $(document).ready(function(){
         p.textAlign(p.CENTER);
         p.text(yr, getCX(yr), height-45 + 10);
       }
+
+      // draw each region's graph
       for (var r = 0; r < regions.length; r++){
         p.drawGraph(regions[r]);
       }
+
+      // draw the key
       for(var r = 0; r < regions.length; r++){
         var newKeyLabel = new KeyLabel(regions[r],width-170, 50 + 20*r);
         keyLabels.push(newKeyLabel);
         newKeyLabel.draw();
       }
+
+      // check if hovering over the key/selecting a region
       var hoveredLabels = 0;
       for(var k = 0; k < keyLabels.length; k++){
         if( p.mouseX <= (keyLabels[k].x + keyLabels[k].w)
@@ -224,13 +167,19 @@ $(document).ready(function(){
           hoveredLabels++;
         }
       }
-      if(hoveredLabels == 0){
+      if(hoveredLabels == 0){ // if nothing is hovered, reset to no hover
         keyHover = '';
+        drawISOLabel('GLE');
+      } else { // otherwise, draw the label for the region being hovered over
+        drawISOLabel(keyHover);
       }
-      drawISOLabel();
+
+      // unit label for y axis
       p.textAlign(p.LEFT);
       p.fill(200);
-      p.text('Deaths per 100,000 births', 20, height/2-15, 75, 30);
+      p.text('Deaths per 100,000 births', 20, height/2-15, 75, 30); 
+
+      // set up labels for individual graph points
       for (var r = 0; r < regions.length; r++){
         p.drawPointLabels(regions[r]);
       }
@@ -241,7 +190,7 @@ $(document).ready(function(){
       var isoPts = [];
       for (var yr in d){
         var newPt = new DataPoint(yr, d[yr], ISO);
-        pts.push(newPt);
+        pts.push(newPt); // save points globally to check hovering
         isoPts.push(newPt);
       }
       
@@ -254,7 +203,7 @@ $(document).ready(function(){
           }
           p.strokeWeight(2);
           p.noFill();
-          p.line(isoPts[pt-1].cx,isoPts[pt-1].cy,isoPts[pt].cx,isoPts[pt].cy);
+          p.line(isoPts[pt-1].cx,isoPts[pt-1].cy,isoPts[pt].cx,isoPts[pt].cy); // draw connecting segments between point and previous point
         }
       }
       for(var pt = 0; pt < isoPts.length; pt++){
@@ -262,8 +211,54 @@ $(document).ready(function(){
       }
     }
 
+    function drawISOLabel(ISO){
+      var firstYear = '';
+      for(var r = 0; r < regions.length; r++){
+        if(ISO == regions[r]){
+          for(var pt = 0; pt < pts.length; pt++){
+            if(pts[pt].ISO == ISO && pts[pt].year == 1995){
+              firstYear = pts[pt]; // save first year value to calculate % change
+            }
+            if(pts[pt].ISO == ISO && pts[pt].year == 2010){
+              var direction = '';
+              var perc = ((pts[pt].value - firstYear.value)/firstYear.value)*100; // calculate % change
+              if(perc < 0){
+                direction = '-';
+              } else if (perc > 0){
+                direction = '+';
+              }
+              p.fill(pts[pt].color);
+              p.text(iso2name[pts[pt].ISO] + ' ' + direction + Math.abs(Math.round(perc)) + '%', pts[pt].cx + 10, pts[pt].cy - 10, 100, 30);
+            }
+          }
+        }
+      }
+    }
+
+    p.drawPointLabels = function(ISO){
+      var d = data[ISO];
+      var isoLabels = [];
+      for (var yr in d){
+        var newLabel = new PointLabel(yr, d[yr], ISO);
+        isoLabels.push(newLabel);
+      }
+      
+      for(var pt = 0; pt < isoLabels.length; pt++){
+        isoLabels[pt].display();
+      }
+    }
+
+    // set up scale functions
+    function getCX(yr){ 
+      return p.map(yr,1995,2010,0+150,width-200);
+    }
+
+    function getCY(val){
+      return p.map(val,0,800,height-50,0+50);
+    }
+
     p.preload = function(){
-      data = p.loadJSON("../json/MATMORTA.json");
+      data = p.loadJSON("../json/MATMORTA.json"); // load the maternal mortality indicator
       iso2name = p.loadJSON("../region-names.json");
       iso2name['GLE'] = 'Global'; // adding global name
     }
@@ -277,9 +272,7 @@ $(document).ready(function(){
       p.background(255);
 
       pts = [];
-      if(view === 'regions'){
-        p.drawRegions();
-      }
+      p.drawRegions();
     };
 
   };
